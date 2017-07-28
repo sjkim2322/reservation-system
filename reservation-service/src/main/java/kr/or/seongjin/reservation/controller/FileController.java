27ce9c7e9da069ee2137be5cf2f3af0928744c88
KIMSEONGJIN <sjkim2322@naver.com>
2017-07-28 10:19:56 +0900

    김성진 : fileController수정

--- reservation-service/src/main/java/kr/or/seongjin/reservation/controller/FileController.java
+++ reservation-service/src/main/java/kr/or/seongjin/reservation/controller/FileController.java
@@ -23,95 +23,54 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.seongjin.reservation.dao.FileDao;
+import kr.or.seongjin.reservation.service.FileService;
 import kr.or.seongjin.util.FileDto;
 
 @Controller
 @RequestMapping("/files")
 public class FileController {
+	
 
-	@Value("${spring.file.upload-locations}")
-	private String baseDir;
-
-	private FileDao fileDao;
+	private FileService fileService;
 
 	@Autowired
-	public void setFileDao(FileDao fileDao) {
-		this.fileDao = fileDao;
+	public void setFileService(FileService fileService) {
+		this.fileService = fileService;
 	}
 
 	@PostMapping
-	public String create(@RequestParam("title") String title, @RequestParam("file") MultipartFile[] files) {
+	public String create(@RequestParam(required=false) Integer type, @RequestParam("file") MultipartFile[] files) {
 		
-		FileDto fileDto = new FileDto();
-		if (files != null && files.length > 0) {
-
-			// windows 사용자라면 "c:\temp\년도\월\일" 형태의 문자열을 구한다.
-			String formattedDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
-			File f = new File(baseDir + formattedDate);
-			if (!f.exists()) { // 파일이 존재하지 않는다면
-				f.mkdirs(); // 해당 디렉토리를 만든다. 하위폴더까지 한꺼번에 만든다.
-			}
-
-			for (MultipartFile file : files) {
-				fileDto.setContent_type(file.getContentType());
-				fileDto.setFile_name(file.getOriginalFilename());
-				fileDto.setFile_length(file.getSize());
-				fileDto.setSave_file_name(formattedDate + File.separator + UUID.randomUUID().toString()); // 실제 저장되는 파일의
-																											// 상대
-																											// 경로(basedir제외)
-
-				fileDto.setUser_id(1);
-				fileDto.setDelete_flag(0);
-				fileDto.setCreate_date(new Date());
-				fileDto.setModify_date(new Date());
-				fileDao.insert(fileDto);// 실제 파일을 저장함.
-
-				// try-with-resource 구문. close()를 할 필요가 없다. java 7 이상에서 가능
-				try (InputStream in = file.getInputStream();
-						FileOutputStream fos = new FileOutputStream(baseDir + fileDto.getSave_file_name())) {
-					int readCount = 0;
-					byte[] buffer = new byte[512];
-					while ((readCount = in.read(buffer)) != -1) {
-						fos.write(buffer, 0, readCount);
-					}
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			} // for
-		} // if
+		System.out.println(files.length);
+		fileService.addFile(type, files);
+		
+		
 
 		return "redirect:/fileManage";
 	}
 
 
 	@GetMapping(path = "/{id}")
-	public void downloadReservationUserCommentImage(@PathVariable(name = "id") Integer id,
+	public void downloadReservationUserCommentImage(@PathVariable Integer id,
 			HttpServletResponse response) throws IOException {
-		FileDto fileDto;
-		fileDto = fileDao.selectById(id);
+		FileDto fileDto = fileService.selectFileById(id);
 		if(fileDto==null) { 
 			response.sendRedirect("/resources/img/noimage.png");
 			return ;
 		}
 
-
-		response.setHeader("Content-Disposition", "inline; filename=\"" + fileDto.getFile_name() + "\";");
+		response.setHeader("Content-Disposition", "inline; filename=\"" + fileDto.getFileName() + "\";");
 		//구브라우저 미구현.
 		response.setHeader("Content-Transfer-Encoding", "binary");
-		response.setHeader("Content-Type", fileDto.getContent_type());
-		response.setHeader("Content-Length", "" + fileDto.getFile_length());
+		response.setHeader("Content-Type", fileDto.getContentType());
+		response.setHeader("Content-Length", "" + fileDto.getFileLength());
 		response.setHeader("Pragma", "no-cache;");
 		response.setHeader("Expires", "-1;");
 		
-
-		java.io.File readFile = new java.io.File(baseDir+fileDto.getSave_file_name());
-		if (!readFile.exists()) { // 파일이 존재하지 않다면
-			throw new RuntimeException("file not found");
-		}
-
-		FileInputStream fis = null;
+		FileInputStream fis;
+		fis = fileService.getFileInputStream(fileDto.getSaveFileName());
+		
 		try {
-			fis = new FileInputStream(readFile);
 			FileCopyUtils.copy(fis, response.getOutputStream()); // 파일을 저장할때도 사용할 수 있다.
 			response.getOutputStream().flush();
 		} catch (Exception ex) {
--- reservation-service/src/main/java/kr/or/seongjin/util/FileDto.java
+++ reservation-service/src/main/java/kr/or/seongjin/util/FileDto.java
@@ -5,93 +5,72 @@ import java.util.Date;
 public class FileDto {
 	
 	private Integer id;
-	private Integer user_id;
-	private String file_name;
-	private String save_file_name;
-	private Long file_length;
-	private String content_type;
-	private Integer delete_flag;
-	private Date create_date;
-	private Date modify_date;
-	
-	
-	
-	
-	
-	
-	
-	public Integer getUser_id() {
-		return user_id;
-	}
-
-
-	public void setUser_id(Integer user_id) {
-		this.user_id = user_id;
-	}
-
-
-	public Integer getDelete_flag() {
-		return delete_flag;
-	}
-
-
-	public void setDelete_flag(Integer delete_flag) {
-		this.delete_flag = delete_flag;
-	}
-
-
-	public Date getCreate_date() {
-		return create_date;
-	}
-
-
-	public void setCreate_date(Date create_date) {
-		this.create_date = create_date;
-	}
-
-
-	public Date getModify_date() {
-		return modify_date;
-	}
-
-
-	public void setModify_date(Date modify_date) {
-		this.modify_date = modify_date;
-	}
-
-
+	private Integer userId;
+	private String fileName;
+	private String saveFileName;
+	private Long fileLength;
+	private String contentType;
+	private Integer deleteFlag;
+	private Date createDate;
+	private Date modifyDate;
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getFile_name() {
-		return file_name;
+	public Integer getUserId() {
+		return userId;
 	}
-	public void setFile_name(String file_name) {
-		this.file_name = file_name;
+	public void setUserId(Integer userId) {
+		this.userId = userId;
 	}
-	public String getSave_file_name() {
-		return save_file_name;
+	public String getFileName() {
+		return fileName;
 	}
-	public void setSave_file_name(String save_file_name) {
-		this.save_file_name = save_file_name;
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
-	public Long getFile_length() {
-		return file_length;
+	public String getSaveFileName() {
+		return saveFileName;
 	}
-	public void setFile_length(Long file_length) {
-		this.file_length = file_length;
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
 	}
-	public String getContent_type() {
-		return content_type;
+	public Long getFileLength() {
+		return fileLength;
 	}
-	public void setContent_type(String content_type) {
-		this.content_type = content_type;
+	public void setFileLength(Long fileLength) {
+		this.fileLength = fileLength;
+	}
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public Integer getDeleteFlag() {
+		return deleteFlag;
+	}
+	public void setDeleteFlag(Integer deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
 	}
 	
 	
 	
-
+	
+	
+	
 }

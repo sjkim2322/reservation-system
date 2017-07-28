a4e0c5432d287e46993d82883280d6c40af098dd
KIMSEONGJIN <sjkim2322@naver.com>
2017-07-28 22:41:41 +0900

    redirectURI수정

--- reservation-service/src/main/java/kr/or/seongjin/reservation/service/impl/NaverLoginService.java
+++ reservation-service/src/main/java/kr/or/seongjin/reservation/service/impl/NaverLoginService.java
@@ -27,15 +27,16 @@ import kr.or.seongjin.reservation.service.LoginService;
 @Service
 public class NaverLoginService implements LoginService {
 
-	private static final String REDIRECT_URI = "http://localhost:8080/login/callback";
 	private static final String CERTIFICATE_URI = "https://nid.naver.com/oauth2.0/authorize?";
 	private static final String TOKEN_URI = "https://nid.naver.com/oauth2.0/token?";
 	private static final String USER_INFO_URI = "https://openapi.naver.com/v1/nid/me";
 	
 	@Value("${spring.naver.client.id}")
-	private String ClIENT_ID;
+	private String clientId;
 	@Value("${spring.naver.client.secret}")
-	private String ClIENT_SECRET;
+	private String clientSecret;
+	@Value("${spring.naver.redirect}")
+	private String redirectUri;
 	
 	private UserDao userDao;
 
@@ -53,8 +54,8 @@ public class NaverLoginService implements LoginService {
 	public String requestCertification(HttpSession session,String originPath) throws UnsupportedEncodingException {
 		String encodedOriginPath = URLEncoder.encode(originPath,"UTF-8");
 		session.setAttribute("state", getState());
-		String apiUrl = CERTIFICATE_URI + "client_id=" + ClIENT_ID + "&response_type=code" + "&redirect_uri="
-				+ URLEncoder.encode(REDIRECT_URI+"?originPath="+encodedOriginPath, "UTF-8") + "&state=" + session.getAttribute("state");
+		String apiUrl = CERTIFICATE_URI + "client_id=" + clientId + "&response_type=code" + "&redirect_uri="
+				+ URLEncoder.encode(redirectUri+"?originPath="+encodedOriginPath, "UTF-8") + "&state=" + session.getAttribute("state");
 		return apiUrl;
 	}
 
@@ -64,7 +65,7 @@ public class NaverLoginService implements LoginService {
 		RestTemplate restTemplate = new RestTemplate();
 		NaverToken naverToken = restTemplate
 				.getForObject(
-						TOKEN_URI + "client_id=" + ClIENT_ID + "&client_secret=" + ClIENT_SECRET
+						TOKEN_URI + "client_id=" + clientId + "&client_secret=" + clientSecret
 								+ "&grant_type=authorization_code" + "&state=" + state + "&code=" + code,
 						NaverToken.class);
 
